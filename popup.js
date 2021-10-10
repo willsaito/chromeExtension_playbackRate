6538f9d58cd884fb805ebd29c7313032eff424d6
stoiaMillawi <53842518+stoiaMillawi@users.noreply.github.com>
2021-10-10 11:18:27 -0300

    extension working

--- popup.js
+++ popup.js
@@ -1,150 +1,53 @@
 // Initialize button with user's preferred color
 let page = document.getElementById("buttonDiv");
 
-let buttons = [100, 150, 200, 250, 300];
-
-function handleButtonClick(event){
+let Button = function(speed) {
+  this.speed = speed;
+  this.func = function(playbackSpeed) {
+    function setSpeed(media, speed) {
+      if (media.length != 0) {
+        for (var i = 0; i < media.length; i++) {
+          if (media[i] != undefined) {
+            media[i].playbackRate = speed;
+          }
+        }
+      }
+    }
+    let video = document.getElementsByTagName("video");
+    let audio = document.getElementsByTagName("audio");
+    // console.log(`Media speed set to X${playbackSpeed/100}.`);
   
+    setSpeed(video, playbackSpeed/100);
+    setSpeed(audio, playbackSpeed/100);
+  };
 }
+let buttonList = [new Button(100), new Button(150), new Button(200), new Button(250), new Button(300)];
 
-function buildButtons(buttons){
-  for(let buttonElem of buttons){
+function buildButtons(buttons) {
+  for (let buttonElem of buttons) {
     let button = document.createElement("button");
-    button.setAttribute("class", `X${button}`);
-    button.addEventListener("click", handleButtonClick);
+    button.setAttribute("class", `X${buttonElem.speed}`);
+    button.textContent = `X${(buttonElem.speed/100).toFixed(1)}`;
+    
+  // When the button is clicked, inject respective speed change function into current page
+    button.addEventListener("click", async () => {
+      let [tab] = await chrome.tabs.query({
+        active: true,
+        currentWindow: true
+      });
+
+      chrome.scripting.executeScript({
+        
+        target: {
+          tabId: tab.id, allFrames:true
+        },
+        function: buttonElem.func,
+        args: [buttonElem.speed],
+      });
+    });
     page.appendChild(button);
   }
 }
 
-// When the button is clicked, inject respective speed change function into current page
-buttonX100.addEventListener("click", async () => {
-  // console.log("extensao") ;
-  let [tab] = await chrome.tabs.query({
-    active: true,
-    currentWindow: true
-  });
+buildButtons(buttonList);
 
-  chrome.scripting.executeScript({
-    target: {
-      tabId: tab.id
-    },
-    function: setPlaybackRate100,
-  });
-});
-buttonX150.addEventListener("click", async () => {
-  // console.log("extensao") ;
-  let [tab] = await chrome.tabs.query({
-    active: true,
-    currentWindow: true
-  });
-
-  chrome.scripting.executeScript({
-    target: {
-      tabId: tab.id
-    },
-    function: setPlaybackRate150,
-  });
-});
-buttonX200.addEventListener("click", async () => {
-  // console.log("extensao") ;
-  let [tab] = await chrome.tabs.query({
-    active: true,
-    currentWindow: true
-  });
-
-  chrome.scripting.executeScript({
-    target: {
-      tabId: tab.id
-    },
-    function: setPlaybackRate200,
-  });
-});
-buttonX250.addEventListener("click", async () => {
-  // console.log("extensao") ;
-  let [tab] = await chrome.tabs.query({
-    active: true,
-    currentWindow: true
-  });
-
-  chrome.scripting.executeScript({
-    target: {
-      tabId: tab.id
-    },
-    function: setPlaybackRate250,
-  });
-});
-
-// The body of this function will be executed as a content script inside the
-// current page
-function setPlaybackRate100() {
-  function setSpeed(media, speed) {
-    if (media.length != 0) {
-      for (var i = 0; i < media.length; i++) {
-        if (media[i] != undefined) {
-          media[i].playbackRate = speed;
-        }
-      }
-
-    }
-  }
-  let video = document.getElementsByTagName("video");
-  let audio = document.getElementsByTagName("audio");
-  setSpeed(video, 1);
-  setSpeed(audio, 1);
-
-}
-function setPlaybackRate150() {
-  function setSpeed(media, speed) {
-    if (media.length != 0) {
-      for (var i = 0; i < media.length; i++) {
-        if (media[i] != undefined) {
-          media[i].playbackRate = speed;
-        }
-      }
-
-    }
-  }
-  let video = document.getElementsByTagName("video");
-  let audio = document.getElementsByTagName("audio");
-  setSpeed(video, 1.5);
-  setSpeed(audio, 1.5);
-
-}
-
-function setPlaybackRate200() {
-  function setSpeed(media, speed) {
-    if (media.length != 0) {
-      for (var i = 0; i < media.length; i++) {
-        if (media[i] != undefined) {
-          media[i].playbackRate = speed;
-        }
-      }
-
-    }
-  }
-  let video = document.getElementsByTagName("video");
-  let audio = document.getElementsByTagName("audio");
-  setSpeed(video, 2);
-  setSpeed(audio, 2);
-
-}
-
-function setPlaybackRate250() {
-  function setSpeed(media, speed) {
-    if (media.length != 0) {
-      for (var i = 0; i < media.length; i++) {
-        if (media[i] != undefined) {
-          media[i].playbackRate = speed;
-        }
-      }
-
-    }
-  }
-  let video = document.getElementsByTagName("video");
-  let audio = document.getElementsByTagName("audio");
-  setSpeed(video, 2.5);
-  setSpeed(audio, 2.5);
-
-  // window.alert("Playback rate is 2.0x.");
-
-}
