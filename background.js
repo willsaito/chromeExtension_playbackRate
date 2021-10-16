fbd4eea4df7428e8804e994cb4eaa12c2650647a
stoiaMillawi <53842518+stoiaMillawi@users.noreply.github.com>
2021-10-16 09:31:31 -0300

    save default buttons to chrome.storage.sync

--- background.js
+++ background.js
@@ -1,2 +1,13 @@
+class Button{
+    constructor(speed, backgroundColor){
+        this.speed = speed;
+        this.backgroundColor = backgroundColor;
+    }
+}
 
+let buttons = [new Button(100, "#4287f5"), new Button(125, "#23afad"), new Button(150, "#35bd4c"),
+ new Button(200, "#f5c242"), new Button(250, "#f57b42"), new Button(300, "#eb4034")]; 
 
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ buttons: buttons });
+});
--- popup.js
+++ popup.js
@@ -1,9 +1,9 @@
-// Initialize button with user's preferred color
-let page = document.getElementById("buttonDiv");
+// Read stored data
+chrome.storage.sync.get("buttons", ({buttons}) => {
 
-let Button = function(speed) {
-  this.speed = speed;
-  this.func = function(playbackSpeed) {
+  let page = document.getElementById("buttonDiv");
+ 
+  function injectFunction(playbackSpeed) {
     function setSpeed(media, speed) {
       if (media.length != 0) {
         for (var i = 0; i < media.length; i++) {
@@ -16,39 +16,42 @@ let Button = function(speed) {
     let video = document.getElementsByTagName("video");
     let audio = document.getElementsByTagName("audio");
     // console.log(`Media speed set to X${playbackSpeed/100}.`);
-  
-    setSpeed(video, playbackSpeed/100);
-    setSpeed(audio, playbackSpeed/100);
-  };
-}
-let buttonList = [new Button(100), new Button(125), new Button(150), new Button(200), new Button(250), new Button(300)];
-document.getElementsByTagName("body")[0].style.width = `${50 * buttonList.length}px`;
 
-function buildButtons(buttons) {
-  for (let buttonElem of buttons) {
-    let button = document.createElement("button");
-    button.setAttribute("class", `X${buttonElem.speed}`);
-    button.textContent = `X${(Math.floor(buttonElem.speed/10)/10).toFixed(1)}`;
-    
-  // When the button is clicked, inject respective speed change function into current page
-    button.addEventListener("click", async () => {
-      let [tab] = await chrome.tabs.query({
-        active: true,
-        currentWindow: true
-      });
-
-      chrome.scripting.executeScript({
-        
-        target: {
-          tabId: tab.id, allFrames:true
-        },
-        function: buttonElem.func,
-        args: [buttonElem.speed],
-      });
-    });
-    page.appendChild(button);
+    setSpeed(video, playbackSpeed / 100);
+    setSpeed(audio, playbackSpeed / 100);
   }
-}
+  
+  // let buttonList = [new Button(100), new Button(125), new Button(150), new Button(200), new Button(250), new Button(300)];
+  document.getElementsByTagName("body")[0].style.width = `${50 * buttons.length}px`;
 
-buildButtons(buttonList);
+  function buildButtons(buttons) {
+    for (let buttonElem of buttons) {
+      let button = document.createElement("button");
+      button.setAttribute("class", `X${buttonElem.speed}`);
+      button.textContent = `X${(Math.floor(buttonElem.speed/10)/10).toFixed(1)}`;
+      button.style.backgroundColor = buttonElem.backgroundColor;
 
+      // When the button is clicked, inject respective speed change function into current page
+      button.addEventListener("click", async () => {
+        let [tab] = await chrome.tabs.query({
+          active: true,
+          currentWindow: true
+        });
+
+        chrome.scripting.executeScript({
+
+          target: {
+            tabId: tab.id,
+            allFrames: true
+          },
+          function: injectFunction,
+          args: [buttonElem.speed],
+        });
+      });
+      page.appendChild(button);
+    }
+  }
+
+  buildButtons(buttons);
+
+});
