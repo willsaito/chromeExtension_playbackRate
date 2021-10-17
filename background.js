08a31e191256ce493ff02339478087ed11dfc69d
stoiaMillawi <53842518+stoiaMillawi@users.noreply.github.com>
2021-10-16 22:50:50 -0300

    ok: customizable buttons

--- background.js
+++ background.js
@@ -5,11 +5,9 @@ class Button{
     }
 }
 
-// let buttons = [new Button(100, "#4287f5"), new Button(125, "#23afad"), new Button(150, "#35bd4c"),
-//  new Button(200, "#f5c242"), new Button(250, "#f57b42"), new Button(300, "#eb4034")]; 
-
 let buttons = [new Button(100, "#4287f5"), new Button(125, "#23afad"), new Button(150, "#35bd4c"),
- new Button(200, "#f5c242"), new Button(250, "#f57b42")]; 
+ new Button(200, "#f5c242"), new Button(250, "#f57b42")/*, new Button(300, "#eb4034")*/]; 
+
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ buttons: buttons });
--- options.js
+++ options.js
@@ -1,8 +1,10 @@
 // Read stored data
-chrome.storage.sync.get("buttons", ({buttons}) => {
-
+chrome.storage.sync.get("buttons", ({
+  buttons
+}) => {
+  let buttonList = buttons;
   let page = document.getElementById("buttonDiv");
- 
+
   function injectFunction(playbackSpeed) {
     function setSpeed(media, speed) {
       if (media.length != 0) {
@@ -20,71 +22,100 @@ chrome.storage.sync.get("buttons", ({buttons}) => {
     setSpeed(video, playbackSpeed / 100);
     setSpeed(audio, playbackSpeed / 100);
   }
-  
-  // let buttonList = [new Button(100), new Button(125), new Button(150), new Button(200), new Button(250), new Button(300)];
-  document.getElementsByTagName("body")[0].style.width = `${50 * buttons.length}px`;
 
-  function buildButtons(buttons) {
-    for (let buttonElem of buttons) {
+  document.getElementsByTagName("body")[0].style.width = `${50 * buttonList.length}px`;
+
+  function buildButtons(buttonList) {
+    page.innerHTML = "";
+    for (let buttonElem of buttonList) {
       let button = document.createElement("button");
-      // button.setAttribute("class", `X${buttonElem.speed}`);
       button.textContent = `X${(Math.floor(buttonElem.speed/10)/10).toFixed(1)}`;
       button.style.backgroundColor = buttonElem.backgroundColor;
 
-      // When the button is clicked, inject respective speed change function into current page
-      button.addEventListener("click", async () => {
-        let [tab] = await chrome.tabs.query({
-          active: true,
-          currentWindow: true
-        });
+      // When the button is clicked, it is removed from the page
+      button.addEventListener("click", (event) => {
+        if (confirm("Deseja remover este botão?")) {
+          let targetElem = event.target;
+          let targetText = targetElem.textContent;
 
-        chrome.scripting.executeScript({
+          //Update stored Buttons
+          for (let i = 0; i < buttonList.length; i++) {
+            if (buttonList[i].speed == buttonElem.speed) {
+              buttonList.splice(i, 1);
+              console.log(buttonList[i]);
+              break;
+            }
+          }
+          targetElem.remove();
 
-          target: {
-            tabId: tab.id,
-            allFrames: true
-          },
-          function: injectFunction,
-          args: [buttonElem.speed],
-        });
+          chrome.storage.sync.set({
+            buttons: buttonList
+          });
+          // button.removeEventListener(event, arguments.callee);
+        }
+
+      });
+
+      button.addEventListener("mouseover", () => {
+        button.style.backgroundColor = "#fc4903";
+        button.textContent = "-";
+      });
+      button.addEventListener("mouseleave", () => {
+        button.textContent = `X${(Math.floor(buttonElem.speed/10)/10).toFixed(1)}`;
+        button.style.backgroundColor = buttonElem.backgroundColor;
       });
       page.appendChild(button);
     }
-    if(buttons.length <6){
-      document.getElementsByTagName("body")[0].style.width = `${50 * (buttons.length + 1)}px`;
+    if (buttonList.length < 6) {
+      document.getElementsByTagName("body")[0].style.width = `${50 * (buttonList.length + 1)}px`;
       let addButton = document.createElement("button");
       addButton.textContent = "+";
       addButton.style.backgroundColor = "#b3b3b3";
-      addButton.addEventListener("click", ()=>{
+      addButton.addEventListener("click", () => {
         let inputForm = document.getElementById("inputForm");
         inputForm.style.display = "block";
       });
 
       page.appendChild(addButton);
-      
-
 
     }
   }
 
-  buildButtons(buttons);
+  buildButtons(buttonList);
 
-});
 
-let confirmButton = document.getElementsByClassName("confirmButton")[0];
+  let numberInput = document.getElementsByClassName("numberInput")[0];
+  numberInput.addEventListener("input", () => {
+    let number = numberInput.value;
+    number = number.replace(/\D/g, "");
+    number = number.replace(/(\d{3})\d/g, "$1");
+    numberInput.value = number;
+  });
+  //Adding new button
+  let confirmButton = document.getElementsByClassName("confirmButton")[0];
+  confirmButton.addEventListener("click", () => {
+    let speedValue = document.getElementsByClassName("numberInput")[0];
+    let colorValue = document.getElementsByClassName("colorInput")[0];
 
-confirmButton.addEventListener("click", ()=>{
-  let speedValue = document.getElementsByClassName("numberInput")[0];
-  let colorValue = document.getElementsByClassName("colorInput")[0];
-  
-  document.getElementById("inputForm").style.display = "none";
-  console.log(`Velocidade: ${speedValue.value} e cor: ${colorValue.value} `);
-});
+    document.getElementById("inputForm").style.display = "none";
 
-let numberInput = document.getElementsByClassName("numberInput")[0];
-numberInput.addEventListener("input", ()=>{
-  let number = numberInput.value;
-  number = number.replace(/\D/g, "");
-  number = number.replace(/(\d{3})\d/g, "$1");
-  numberInput.value = number;
+    buttonList.push({
+      backgroundColor: colorValue.value,
+      speed: parseInt(speedValue.value)
+    });
+    //Sort the array
+    buttonList.sort(function (a, b) {
+      return a.speed - b.speed;
+    })
+    //Update data
+    chrome.storage.sync.set({
+      buttons: buttonList
+    });
+
+  });
+
+  //On data update rebuild buttons
+  chrome.storage.onChanged.addListener(() => {
+    buildButtons(buttonList);
+  });
 });
